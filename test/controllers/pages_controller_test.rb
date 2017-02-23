c85581426524c9b99ddf9844883b09f39f9e0ddd
Tanner Crocker <tec@email.uscupstate.edu>
2017-02-23 00:04:58 +0000

    Removed redundancies

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,24 +1,32 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @appTitle = "Bricked Electronics"
+  end
+  
   test "should get home" do
     get pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@appTitle}"
   end
 
   test "should get help" do
     get pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@appTitle}"
   end
 
   test "should get shop" do
     get pages_shop_url
     assert_response :success
+    assert_select "title", "Shop | #{@appTitle}"
   end
 
   test "should get account" do
     get pages_account_url
     assert_response :success
+    assert_select "title", "Account | #{@appTitle}"
   end
 
 end
