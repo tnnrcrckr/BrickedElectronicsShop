cd560d43ef69c1c919d5c71eb3e12568d51674aa
Tanner Crocker <tec@email.uscupstate.edu>
2017-03-21 14:35:46 +0000

    Added bcrypt gem; Partialized header/footer; Replaced some html tags with embedded ruby tags

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     validates :name, presence: true, length:{ maximum: 50 }
     
-    VALID_EMAIL_REGEX
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 100 },
         format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
         
