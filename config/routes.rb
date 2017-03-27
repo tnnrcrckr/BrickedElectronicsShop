762876e8aed96ee50e39545447ca92de8ad5514c
Tanner Crocker <tec@email.uscupstate.edu>
2017-03-27 00:36:43 +0000

    modified routes to have url & path variables

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
 
   root "pages#home"
-
-  #get 'pages/home'
-  #get 'pages/help'
-  #get 'pages/shop'
-  #get 'pages/account'
-  #get 'pages/about'
-  #get 'pages/navigation'
-  #get 'pages/reviews'
   
   get '/help', to: 'pages#help'
   get '/shop', to: 'pages#shop'
