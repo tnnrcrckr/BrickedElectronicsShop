45dea71b4c82cd6e77efecbfc36ad7796a5b66bf
Tanner Crocker <tec@email.uscupstate.edu>
2017-03-27 23:15:17 +0000

    removed some test.rb deprecation warnings

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   
   validates :name,  presence: true, length: { maximum: 50 }
   
-  validates :email, presence: true, length: { maximum: 255 }, format: {with: VALID_EMAIL_REGEX}, uniqueness: true {case_sensitive: false}
+  validates :email, presence: true, length: { maximum: 255 },
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: { case_sensitive: false }
   
   
 end
