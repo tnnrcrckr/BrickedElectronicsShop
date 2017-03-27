9c16aaefe2b6bded91df0c4aebe7af1b42ff9a09
Tanner Crocker <tec@email.uscupstate.edu>
2017-03-27 04:21:01 +0000

    Changed header/footer partials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def isLoggedIn?
+    true
+  end
+  
   def create
     @user = User.new(user_params)
     if @user.save
