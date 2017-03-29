e29fbceb22b4d6567100d5f1321d31508013e9ee
Tanner Crocker <tec@email.uscupstate.edu>
2017-03-29 17:58:57 +0000

    Successful migration and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315224002) do
+ActiveRecord::Schema.define(version: 20170327214426) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
