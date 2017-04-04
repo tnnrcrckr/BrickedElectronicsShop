4491e5c29afc97968e2f525961380302445a4672
Tanner Crocker <tec@email.uscupstate.edu>
2017-04-04 06:13:36 +0000

    Created sessions

--- app/assets/javascripts/account_form.js
+++ app/assets/javascripts/account_form.js
@@ -1,31 +1,17 @@
-function toggle(el, on)
-{
-    if(el.style != null)
-        if(on == 1)
-        {
-            el.style.display = "block";
-            console.log("Show " + el + ".");
-        }
-        else
-        {
-            el.style.display = "none";
-            console.log("Hide " + el + ".");
-        }
-    return;
-}
-
-function formButtonBackgrounds(el, on)
-{
-    if(el.style != null)
-        if(on == 1)
-        {
-            el.style.backgroundColor = "#3e3";
-            console.log("Color " + el + ".");
-        }
-        else
-        {
-            el.style.backgroundColor = "inherit";
-            console.log("Decolor " + el + ".");
-        }
+function toggle_acc(el)
+{    
+    if(el == "login_form")
+    {
+        document.getElementById("acc_h1").innerHTML = "Login Page"
+        document.getElementById("signup_form").style.display = "none"
+        document.getElementById(el).style.display = "block"
+    }
+    else if(el == "signup_form")
+    {
+        document.getElementById("acc_h1").innerHTML = "Signup Page"
+        document.getElementById("login_form").style.display = "none"
+        document.getElementById(el).style.display = "block"
+    }
+        
     return;
 }
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       flash.now[:success] = 'Welcome, ' + user[:name]
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash[:danger] = 'Invalid email/password combination'
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
