11d007f97c553825180ff2250e226b6fa22855f9
Tanner Crocker <tec@email.uscupstate.edu>
2017-04-04 04:10:37 +0000

    basic login fin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to Bricked Electronics"
       redirect_to @user
     else
@@ -21,6 +22,7 @@ class UsersController < ApplicationController
   def account
     new
   end
+  
   private
   
     def user_params
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,12 @@ module SessionsHelper
     
     # Returns true if the user is logged in, false otherwise.
     def logged_in?
-        !current_user.nil?
+        !@current_user.nil?
+    end
+    
+    # Logs out the current user.
+    def log_out
+        session.delete(:user_id)
+        @current_user = nil
     end
 end
