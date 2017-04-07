db5a1aca057b749d0e54b79a8007a9fe9fca2e74
Tanner Crocker <tec@email.uscupstate.edu>
2017-04-07 04:06:27 +0000

    Activations ready!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,12 @@ class UsersController < ApplicationController
   end
   
   def index
-    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless FILL_IN
+    redirect_to root_url and return unless @user.activated
   end
   
   def create
