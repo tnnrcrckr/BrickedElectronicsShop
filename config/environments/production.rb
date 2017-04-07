bb8ddb7fcfb07ea5b4d4828bb037e8d7e157267c
Tanner Crocker <tec@email.uscupstate.edu>
2017-04-07 03:40:38 +0000

    Activations ready?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'bricked-electronics-shop.herokuapp.com'
+  host = 'https://bricked-electronics-shop.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
